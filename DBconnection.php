680e9f7207e58617945269586f4f0dc8083717e0
IsratKurinka <israt.cse20@gmail.com>
2017-05-07 17:11:04 +0600

    First commit

---
+++ DBconnection.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emma
+ * Date: 4/24/17
+ * Time: 2:52 PM
+ */
+//session_start();
+$servername = "localhost";
+$username = "duhospital";
+$password = "duhospital";
+$dbname = "duhospital";
+
+//create connection
+$db = new mysqli($servername, $username, $password, $dbname);
+
+//check connection
+if ($db -> connect_error){
+    die("connection failed ".$db->connect_error);
+}
+else{
+    //echo "connection successful";
+}
+?>
---
+++ homeIsLoggedin.js
@@ -0,0 +1,25 @@
+/**
+ * Created by shafa on 5/6/17.
+ */
+
+function isLoggedin2() {
+    //alert("ckecking2");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        console.log(this.readyState);
+        if(this.readyState == 4 && this.status == 200) {
+            console.log(this.status);
+            var response = xhttp.responseText;
+            //alert(response);
+            if( String(response.trim()) === "yes" ){
+                document.getElementById("navBarHealthcard").style.display = 'none';
+                document.getElementById("navBarLog").innerHTML = '<li>\<a onclick="myFunc();"' +
+                    ' style="width:auto;"> Logout </a>\</li>';
+                //document.getElementById("id01").style.display = "none";
+            }
+        }
+    }
+    xhttp.open("GET", "checkSession.php", true);
+    xhttp.send();
+}
+isLoggedin2();
